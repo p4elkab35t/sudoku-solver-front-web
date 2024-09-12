2c35cccc06d23d53aa5a6c984be27dfbc79b0ce6
p4elkab35t <saganiter234@gmail.com>
2024-09-11 20:12:56 -0700

    fixing bug with prop sending

--- src/components/sudoku/Board.tsx
+++ src/components/sudoku/Board.tsx
@@ -1,17 +1,21 @@
 // import React from 'react';
 import './Board.css';
 import Cell from './Cell';
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-interface Props {
+interface BoardProps {
     boardSize?: number;
 }
 
-const Board: React.FC<Props> = ({boardSize = 9}) => {
+const Board: React.FC<BoardProps> = ({boardSize = 9}) => {
 
 
-    const cells = Array.from({ length: boardSize * boardSize });
-    cells.fill(<Cell />);
+    const cells: Array<ReactNode> = [];
+    for (let i = 0; i < boardSize * boardSize; i++) {
+        // console.log(i);
+        cells.push(<Cell cellKey={i} />);
+        console.log(cells);
+    }
 
     return (
         <div className="board">
--- src/components/sudoku/Cell.tsx
+++ src/components/sudoku/Cell.tsx
@@ -1,8 +1,17 @@
 import "./Cell.css";
+import React from "react";
 
-function Cell(){
+interface CellProps {
+    cellKey: number;
+}
+
+const Cell: React.FC<CellProps> = ({cellKey}) => {
+
+    const id: string = cellKey.toString();
+
     return (
-        <div className="cell">
+        console.log(id, " created"),
+        <div className="cell" id={id}>
             <input type="text" maxLength={1} />
         </div>
     )
